3a892f7baf63516bcff64b7e2bcd5c1b95dbe488
Aidan Taha <botatooo@protonmail.com>
2023-09-14 00:51:25 -0400

    fix: umd complaining about react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
