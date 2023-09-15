b56c6e08dc933565bede24d5ade7436fe57800c2
botato <63275405+botatooo@users.noreply.github.com>
2023-09-15 16:04:22 -0400

    make about me page text smaller

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -4,22 +4,23 @@ export default function About() {
   return (
     <div className="m-auto">
       <div className="border border-neutral-300 rounded-2xl p-16">
-        <h1 className="text-4xl font-bold text-center mb-5">À propos</h1>
-        <p className="text-2xl">Generatrice de bibliographie</p>
-        <p className="text-2xl">
+        <h1 className="text-2xl font-bold text-center mb-5">À propos</h1>
+        <p className="text-l">Generatrice de bibliographie</p>
+        <p className="text-l">
           Version{" "}
           {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ? (
             <a
-              href={`https://github.com/botatooo/biblio-generator/tree/${process.env.VERCEL_GIT_COMMIT_SHA}`}
+              href={`https://github.com/botatooo/biblio-generator/tree/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`}
+              className="text-blue-500 hover:underline"
             >
-              {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA.slice(0, 5)}
+              {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA.slice(0, 6)}
             </a>
           ) : (
             "inconnue"
           )}
         </p>
         <br />
-        <p className="text-2xl">
+        <p className="text-l">
           Auteur: Adnan-Aidan Taha &quot;botato&quot;{" "}
           <a href="https://github.com/botatooo">
             <img
@@ -38,8 +39,8 @@ export default function About() {
             />
           </a>
         </p>
-        <p className="text-2xl">Contributeurs:</p>
-        <ul className="text-2xl list-disc">
+        <p className="text-l">Contributeurs:</p>
+        <ul className="text-l list-disc">
           <li className="ml-8">
             Bill Xu &quot;BX&quot;
             <a href="https://github.com/bx07">
@@ -53,7 +54,7 @@ export default function About() {
           </li>
         </ul>
         <br />
-        <p className="text-2xl">
+        <p className="text-l">
           Le code source est disponible sur{" "}
           <a
             href="https://github.com/botatooo/biblio-generator"
@@ -68,7 +69,7 @@ export default function About() {
             GitHub
           </a>
         </p>
-        <p className="text-2xl">
+        <p className="text-l">
           Ce site est hébergé sur{" "}
           <a
             href="https://vercel.com"
