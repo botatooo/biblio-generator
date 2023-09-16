a9723332dc54afe0deb39fb045fffb0bb26ce9c4
Aidan Taha <botatooo@protonmail.com>
2023-09-16 10:46:39 -0400

    fix: about me page styling (again)

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -3,8 +3,8 @@
 export default function About() {
   return (
     <div className="m-auto">
-      <div className="border border-neutral-300 rounded-2xl p-16">
-        <h1 className="text-2xl font-bold text-center mb-5">À propos</h1>
+      <div className="border border-neutral-300 rounded-2xl p-8">
+        <h1 className="text-2xl font-bold text-center mb-3">À propos</h1>
         <p className="text-l">Generatrice de bibliographie</p>
         <p className="text-l">
           Version{" "}
@@ -22,7 +22,7 @@ export default function About() {
         <br />
         <p className="text-l">
           Auteur: Adnan-Aidan Taha &quot;botato&quot;{" "}
-          <a href="https://github.com/botatooo">
+          <a href="https://github.com/botatooo" target="_blank">
             <img
               alt="GitHub"
               src="https://skillicons.dev/icons?i=github"
@@ -34,13 +34,13 @@ export default function About() {
         <p className="text-l">Contributeurs:</p>
         <ul className="text-l list-disc">
           <li className="ml-8">
-            Bill Xu &quot;BX&quot;
-            <a href="https://github.com/bx07">
+            Bill Xu &quot;BX&quot;{" "}
+            <a href="https://github.com/bx07" target="_blank">
               <img
                 alt="GitHub"
                 src="https://skillicons.dev/icons?i=github"
                 loading="lazy"
-                className="w-6 h-6 ml-1 mb-1 inline-block"
+                className="w-6 h-6 inline-block"
               />
             </a>
           </li>
