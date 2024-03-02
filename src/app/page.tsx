ca8be539c0969f9583a3117f15858164c5014895
Aidan T <63275405+botatooo@users.noreply.github.com>
2024-03-02 18:57:21 -0500

    fix: essaie #2 pour réparer l'erreur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         if (!Array.isArray(data) || data.length === 0) return;
 
         // https://stackoverflow.com/a/9645447/19456595
-        setBiblio((prev) => [...prev, ...data].sort((a, b) => a.content.toLowerCase().localeCompare(b.content.toLowerCase())));
+        setBiblio((prev) => [...prev, ...data].sort((a, b) => a.content.author.toLowerCase().localeCompare(b.content.author.toLowerCase())));
         setIsLoading(false);
       })
       .catch(console.error);
