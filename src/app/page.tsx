7aa9c50fc6d8532ea3a3ebfb3c7370c414cc13b4
Aidan T <63275405+botatooo@users.noreply.github.com>
2024-03-30 01:32:34 -0400

    Revert "feat: ajouter le guide de pres des travaux ecrits"
    This reverts commit 46058e651458c31e2f8e5ff98e8cb5e65acb61de.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,141 +68,136 @@ export default function Home() {
   }, [links]);
 
   return (
-    <div className="w-full max-w-full">
-      <div className="py-2 px-4 mb-2 text-white text-center bg-teal-400 rounded-md">
-        Voir le{" "}<a  className="underline" href="/fichiers/Guide_prés_travaux_écrits.pdf" target="_blank">Guide de présentation des travaux écrits</a>{" "} pour plus d&apos;information.
-      </div>
-      <div className="grid lg:grid-cols-2 gap-8">
-        {initialized ? (
-          isMobile ? (
-            <div>
-              <div className="border border-neutral-300 rounded-md">
-                <table className="w-full overflow-hidden break-all">
-                  <thead>
-                    <tr className="rounded-t-md">
-                      <th className="text-center font-medium py-2">Liens</th>
-                    </tr>
-                  </thead>
-                  <tbody className={clsx(roboto_mono.variable, "font-mono")}>
-                    {links.length > 0 ? (
-                      links.map((link, index) => (
-                        <tr
-                          key={index}
-                          className="border-neutral-300 border-t even:bg-teal-100"
-                        >
-                          <td className="p-2">{link}</td>
-                        </tr>
-                      ))
-                    ) : (
-                      <tr className="border-neutral-300 border-t even:bg-teal-100">
-                        <td className="p-2">Ajoutez un lien pour commencer!</td>
+    <div className="grid lg:grid-cols-2 gap-8 w-full max-w-full">
+      {initialized ? (
+        isMobile ? (
+          <div>
+            <div className="border border-neutral-300 rounded-md">
+              <table className="w-full overflow-hidden break-all">
+                <thead>
+                  <tr className="rounded-t-md">
+                    <th className="text-center font-medium py-2">Liens</th>
+                  </tr>
+                </thead>
+                <tbody className={clsx(roboto_mono.variable, "font-mono")}>
+                  {links.length > 0 ? (
+                    links.map((link, index) => (
+                      <tr
+                        key={index}
+                        className="border-neutral-300 border-t even:bg-teal-100"
+                      >
+                        <td className="p-2">{link}</td>
                       </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
-              <div className="mt-4 border-neutral-300 border rounded-md">
-                <input
-                  name="url"
-                  type="url"
-                  tabIndex={0}
-                  onKeyDown={(e) => {
-                    if (e.key !== "Enter") return;
-                    e.preventDefault();
-
-                    if (addLink(e.currentTarget.value.trim())) {
-                      e.currentTarget.value = "";
-                    }
-                  }}
-                  className="h-8 w-[calc(100%-5rem)] bg-none rounded-md"
-                />
-                <button
-                  onClick={(e) => {
-                    const input = e.currentTarget.previousElementSibling as
-                      | HTMLInputElement
-                      | undefined;
-                    if (!input) return;
-
-                    if (addLink(input.value.trim())) {
-                      input.value = "";
-                    }
-                  }}
-                  className="bg-teal-300 h-8 w-20 p-1 border rounded-md hover:bg-teal-200 float-right"
-                >
-                  Ajouter
-                </button>
-              </div>
+                    ))
+                  ) : (
+                    <tr className="border-neutral-300 border-t even:bg-teal-100">
+                      <td className="p-2">Ajoutez un lien pour commencer!</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
             </div>
-          ) : (
-            <div>
-              <div className="border border-neutral-300 rounded-md">
-                <h1 className="text-center font-medium py-2">Liens</h1>
-                <textarea
-                  name="urls"
-                  tabIndex={0}
-                  className="h-64 w-full border-t border-neutral-200"
-                />
-              </div>
-              <button
-                className="bg-teal-300 mt-2 py-1 px-2 border rounded-md hover:bg-teal-200 float-right"
-                onClick={() => {
-                  const textarea = document.querySelector(
-                    "textarea[name=urls]"
-                  ) as HTMLTextAreaElement | null;
-                  console.log(textarea, textarea?.value);
-                  if (!textarea) return;
+            <div className="mt-4 border-neutral-300 border rounded-md">
+              <input
+                name="url"
+                type="url"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                  if (e.key !== "Enter") return;
+                  e.preventDefault();
 
-                  const urls = textarea.value
-                    .split("\n")
-                    .map((url) => url.trim())
-                    .filter((url) => url !== "");
-
-                  if (addLinks(urls)) {
-                    textarea.value = "";
+                  if (addLink(e.currentTarget.value.trim())) {
+                    e.currentTarget.value = "";
                   }
                 }}
+                className="h-8 w-[calc(100%-5rem)] bg-none rounded-md"
+              />
+              <button
+                onClick={(e) => {
+                  const input = e.currentTarget.previousElementSibling as
+                    | HTMLInputElement
+                    | undefined;
+                  if (!input) return;
+
+                  if (addLink(input.value.trim())) {
+                    input.value = "";
+                  }
+                }}
+                className="bg-teal-300 h-8 w-20 p-1 border rounded-md hover:bg-teal-200 float-right"
               >
-                Ajouter les liens à la bibliographie
+                Ajouter
               </button>
             </div>
-          )
-        ) : (
-          <div />
-        )}
-        <div className="block  border-neutral-300 rounded-md border p-4 max-w-full overflow-hidden break-words">
-          <div className="font-['Times_New_Roman'] text-[12px]">
-            <center>
-              <strong>Bibliographie</strong>
-              <SpinnerCircular
-                enabled={isLoading}
-                className="ml-2 inline-block"
-                size={15}
-                thickness={300}
-                color="rgb(20 184 166)" // teal-500
-                secondaryColor="transparent"
-              />
-            </center>
-            <br />
-            <strong>Document Internet</strong>
-            <br />
-            {biblio.map(({ success, content }, _index) => (
-              <>
-                {success === true ? (
-                  <span>
-                    {content.author}. (Page consultée le {new Date().toLocaleDateString("fr-CA", { year: "numeric", month: "long", day: "numeric" })}).{" "}
-                    <i>{content.title}</i>, [En ligne]. Adresse URL :{" "}
-                    <a href={content.url} className="underline text-sky-700">
-                      {content.url}
-                    </a>
-                  </span>
-                ) : (
-                  // </Markdown>
-                  <span className="text-red-500">{content}</span>
-                )}
-                <br />
-              </>
-            ))}
           </div>
+        ) : (
+          <div>
+            <div className="border border-neutral-300 rounded-md">
+              <h1 className="text-center font-medium py-2">Liens</h1>
+              <textarea
+                name="urls"
+                tabIndex={0}
+                className="h-64 w-full border-t border-neutral-200"
+              />
+            </div>
+            <button
+              className="bg-teal-300 mt-2 py-1 px-2 border rounded-md hover:bg-teal-200 float-right"
+              onClick={() => {
+                const textarea = document.querySelector(
+                  "textarea[name=urls]"
+                ) as HTMLTextAreaElement | null;
+                console.log(textarea, textarea?.value);
+                if (!textarea) return;
+
+                const urls = textarea.value
+                  .split("\n")
+                  .map((url) => url.trim())
+                  .filter((url) => url !== "");
+
+                if (addLinks(urls)) {
+                  textarea.value = "";
+                }
+              }}
+            >
+              Ajouter les liens à la bibliographie
+            </button>
+          </div>
+        )
+      ) : (
+        <div />
+      )}
+      <div className="block  border-neutral-300 rounded-md border p-4 max-w-full overflow-hidden break-words">
+        <div className="font-['Times_New_Roman'] text-[12px]">
+          <center>
+            <strong>Bibliographie</strong>
+            <SpinnerCircular
+              enabled={isLoading}
+              className="ml-2 inline-block"
+              size={15}
+              thickness={300}
+              color="rgb(20 184 166)" // teal-500
+              secondaryColor="transparent"
+            />
+          </center>
+          <br />
+          <strong>Document Internet</strong>
+          <br />
+          {biblio.map(({ success, content }, _index) => (
+            <>
+              {success === true ? (
+                <span>
+                  {content.author}. (Page consultée le {new Date().toLocaleDateString("fr-CA", { year: "numeric", month: "long", day: "numeric" })}).{" "}
+                  <i>{content.title}</i>, [En ligne]. Adresse URL :{" "}
+                  <a href={content.url} className="underline text-sky-700">
+                    {content.url}
+                  </a>
+                </span>
+              ) : (
+                // </Markdown>
+                <span className="text-red-500">{content}</span>
+              )}
+              <br />
+            </>
+          ))}
         </div>
       </div>
     </div>
